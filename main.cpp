465f4ec363691e34dda5a5b8d926566442c921f1
gabrielpona <sana.gp@gmail.com>
2021-01-04 20:15:43 -0300

    Testing some things

--- main.cpp
+++ main.cpp
@@ -1,10 +1,41 @@
 #include <cstdlib>
+#include <iostream>
+#include <conio.h>
+#include <vector>
+
+#include "lib/SystemTools.h"
 
 using namespace std;
+
+void mainMenu(SystemTools &systemTools){
+    
+    system("cls");
+    
+    cout << "Select the drive that contais the \"users\" directory:\n\n";
+    
+    char driveList[] = "CDE";
+            
+    for(int i=0;i<3;i++){
+        
+        cout << "\t\t\t[" << (i+1) << "]     Drive " << driveList[i] << ":\n";        
+    }
+    
+    cout << "\n\t\tOr type\t[0]     to Exit\n"; 
+    
+    cin >> driveList;
+    
+            
+}
 
 
 int main(int argc, char** argv) {
 
+    SystemTools systemTools = SystemTools();
+    
+    mainMenu(systemTools);
+    
+    
+            
     return 0;
 }
 
